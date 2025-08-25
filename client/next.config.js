43f73810150194ccb7300b436bb6ef1447a8039d
Hector Archer <hectorae28@gmail.com>
2025-08-24 21:17:09 -0400

    fix info

--- client/next.config.js
+++ client/next.config.js
@@ -2,4 +2,17 @@ module.exports = {
   experimental: {
     serverActions: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
